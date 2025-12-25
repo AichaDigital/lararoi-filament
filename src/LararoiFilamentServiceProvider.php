865ff6135b5d1d0d982cb797d8c5915460797e88
Abdelkarim Mateos Sanchez <abdelkarim.mateos@castris.com>
2025-12-25 15:28:59 +0100

    Pint mistakes

--- src/LararoiFilamentServiceProvider.php
+++ src/LararoiFilamentServiceProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace AichaDigital\LararoiFilament;
 
-use AichaDigital\LararoiFilament\Commands\LararoiFilamentCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
